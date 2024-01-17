5e97bbef9d7ce9ee5c2a1d5a70d13421b1ebd496
nanangcreacoll <nanang.creacol.26@gmail.com>
2024-01-18 03:12:53 +0800

    feat: create models

---
+++ app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Contact;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class User extends Model
+{
+    protected $table = "users";
+    protected $primaryKey = "id";
+    protected $keyType = "int";
+    public $timestamps = true;
+    public $incrementing = true;
+
+    public function contacts() : HasMany {
+        return $this->hasMany(Contact::class, "user_id", "id");
+    }
+}
---
+++ database/migrations/2024_01_17_190158_create_addresses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string("street", 255)->nullable();
+            $table->string("city", 255)->nullable();
+            $table->string("province", 255)->nullable();
+            $table->string("country", 255)->nullable(false);
+            $table->string("postal_code", 63)->nullable();
+            $table->unsignedBigInteger("contact_id")->nullable(false);
+            $table->timestamps();
+            $table->foreign("contact_id")->on("contacts")->references("id");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
